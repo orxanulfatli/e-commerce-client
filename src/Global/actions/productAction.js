ece3c285ecb0fdd1660a478b1cd18102f0b68522
Orxan Ulfatli <orxanulfatli@gmail.com>
2023-07-18 18:02:57 +0400

    fix

--- src/Global/actions/productAction.js
+++ src/Global/actions/productAction.js
@@ -160,7 +160,7 @@ export const getProductDetails = (id) => async (dispatch) => {
   try {
     dispatch({ type: PRODUCT_DETAILS_REQUEST });
 
-    const { data } = await axios.get(`${render}/api/v1/product/${id}`);
+    const { data } = await axios.get(`${apiUrl}/api/v1/product/${id}`);
 
     dispatch({
       type: PRODUCT_DETAILS_SUCCESS,
